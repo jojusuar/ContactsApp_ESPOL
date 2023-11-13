380657324e584d5113256e1494e9e453b44cd82b
Adrian Delgado <git@adrianjdelgado.com>
2023-11-13 11:28:41 -0500

    Remove redundant loadContacts

--- src/main/java/persistence/Memory.java
+++ src/main/java/persistence/Memory.java
@@ -21,25 +21,27 @@ public class Memory {
     public static ArrayList<User> getUsers() {
         return users;
     }
-    
-    public static void addUser(User user){
+
+    public static void addUser(User user) {
         users.addLast(user);
     }
-    
-    public static void deleteUser(User user){
+
+    public static void deleteUser(User user) {
         users.remove(user);
     }
-    
-    public static void save(){
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("src/main/resources/memory/users.ser"));) {
+
+    public static void save() {
+        try (ObjectOutputStream out = new ObjectOutputStream(
+                new FileOutputStream("src/main/resources/memory/users.ser"));) {
             out.writeObject(users);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-    
-    public static void load(){
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("src/main/resources/memory/users.ser"));) {
+
+    public static void load() {
+        try (ObjectInputStream in = new ObjectInputStream(
+                new FileInputStream("src/main/resources/memory/users.ser"));) {
             users = (ArrayList<User>) in.readObject();
             System.out.println(users);
         } catch (IOException ex) {
